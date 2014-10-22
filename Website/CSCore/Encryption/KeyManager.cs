270c514d5f66776853847eb7187d40da82cbe7da
CONVERSION\Ravi <CONVERSION\Ravi>
2014-10-22 23:30:57 +0000

    Key Manager RollBack

--- Website/CSCore/Encryption/KeyManager.cs
+++ Website/CSCore/Encryption/KeyManager.cs
@@ -11,8 +11,8 @@ namespace CSCore.Encryption
     {
         public static string GetEncryptionKey()
         {
-            //string encryptionKeyPath = @"C:\Windows\System32\oem\debug\debugInfo.txt";//  ConfigHelper.ReadAppSetting("EncryptionKeyPath", "");
-            return "eIvQ/95MdZdqN1KvEhnRoWm5o/Ets/RwX11ORZZBOaF73iT4fAURobwsP9YSHA5N";
+            string encryptionKeyPath = @"C:\Windows\System32\oem\debug\debugInfo.txt";//  ConfigHelper.ReadAppSetting("EncryptionKeyPath", "");
+            return File.ReadAllLines(encryptionKeyPath)[1];
         }
     }
 }
