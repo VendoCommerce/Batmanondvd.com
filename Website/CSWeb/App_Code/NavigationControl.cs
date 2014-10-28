968dfc2aa59c4d142152b9ffc0b09cfe3a7244f1
CONVERSION\mhaeri <CONVERSION\mhaeri>
2014-10-28 01:28:58 +0000

    Disable no-cache

--- Website/CSWeb/App_Code/NavigationControl.cs
+++ Website/CSWeb/App_Code/NavigationControl.cs
@@ -42,11 +42,11 @@ namespace CSWeb.App_Code
 
         public static void DisableClientPageCache()
         {
-            HttpContext.Current.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
-            HttpContext.Current.Response.Cache.SetValidUntilExpires(false);
-            HttpContext.Current.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
-            HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
-            HttpContext.Current.Response.Cache.SetNoStore();
+            ////HttpContext.Current.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            ////HttpContext.Current.Response.Cache.SetValidUntilExpires(false);
+            ////HttpContext.Current.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            ////HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            ////HttpContext.Current.Response.Cache.SetNoStore();
         }
 
     }
